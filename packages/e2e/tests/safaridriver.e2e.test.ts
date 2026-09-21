7710c51c4bcb7dc701284c283de65cd2239f3297
dprevost-perso <dprevost-github@outlook.com>
2026-09-20 21:54:40 -0400

    Add safari e2e

---
+++ packages/e2e/tests/safaridriver.e2e.test.ts
@@ -0,0 +1,27 @@
+import waitPort from 'wait-port'
+import { remote } from 'webdriverio'
+import { describe, it } from 'vitest'
+
+import { start, stop } from 'safaridriver'
+
+describe.skipIf(process.platform !== 'darwin')('Safaridriver E2E Tests', () => {
+    it('start safaridriver manually', async () => {
+        const port = 4444
+        start({ port })
+
+        try {
+            await waitPort({ port })
+            const browser = await remote({
+                automationProtocol: 'webdriver',
+                port,
+                capabilities: {
+                    browserName: 'safari'
+                }
+            })
+            await browser.url('https://webdriver.io')
+            await browser.deleteSession()
+        } finally {
+            stop()
+        }
+    })
+})
